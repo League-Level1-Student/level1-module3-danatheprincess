ccee52d83127d3c9462d2644047b78b9d7ece7d3
jaLeague <jackie.talbot@jointheleague.org>
2021-10-12 18:26:54 -0700

    Update GridworldRunner.java

--- src/_06_gridworld/GridworldRunner.java
+++ src/_06_gridworld/GridworldRunner.java
@@ -1,6 +1,7 @@
-	import java.awt.Color;
-
+package _06_gridworld;
+import java.awt.Color;
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
@@ -11,9 +12,20 @@ public class GridworldRunner {public static void main(String[] args) {
 	Location loc= new Location(0,6);
 	wor.add(loc, insect);
     insect.setColor(Color.BLUE);
-    Bug ins=new Bug();
-	Location place = new Location(3,6);
-	ins.getLocation();
-	wor.add(place, ins);
+    insect.turn();
+    Flower flor=new Flower();
+    Location place=new Location(0,5);
+    wor.add(place, flor);
+    Flower plant=new Flower();
+    Location cat=new Location(0,7);
+    wor.add(cat, plant);
+    for(int i=0;i<10;i++) {
+    	for(int j=0;j<10;j++){
+    	Flower sun=new Flower();
+    	Location beach=new Location(i,j);
+    	wor.add(beach, sun);
+    }
+    	
+    }
 	}
-                             }                      
+}  
